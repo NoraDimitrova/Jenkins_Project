d1c308c57da9940b73c0fc44edc3b9ee79b5ddb0
Nora Dimitrova <nora_dd@abv.bg>
2023-01-01 21:24:13 +0200

    Add Task3

---
+++ Task3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello, the day is " + i);
+
+        }
+    }
+
+}
